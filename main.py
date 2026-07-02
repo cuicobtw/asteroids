2255342d05dc9dd668160d5bf0dde07384131b46
cuicobtw <205841937+cuicobtw@users.noreply.github.com>
2026-07-01 22:50:35 -0300

    feat: restrict the game to 60fps

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt: float = 0.0
 
     while True:
         log_state()
@@ -19,7 +21,9 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
-
+        
+        dt = clock.tick(60) / 1000
+        print(dt)
 
 if __name__ == "__main__":
     main()
